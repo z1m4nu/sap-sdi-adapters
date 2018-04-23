4bf6bc786b460d57b6cd9664d3c87eea30e95aff
e.soden <e.soden@gmail.com>
2018-04-23 17:23:43 +0200

    Initial adds

---
+++ org.crossroad.sdi.adapter/src/org/crossroad/sdi/adapter/JDBCDriverUtil.java
@@ -0,0 +1,13 @@
+package org.crossroad.sdi.adapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class JDBCDriverUtil {
+
+	
+	private Map<String, String> drvInfo = new HashMap<String, String>();
+	
+	
+
+}
---
+++ org.crossroad.sdi.adapter/src/org/crossroad/sdi/adapter/impl/AbstractJDBCAdapterFactory.java
@@ -0,0 +1,8 @@
+package org.crossroad.sdi.adapter.impl;
+
+import com.sap.hana.dp.adapter.sdk.AdapterFactory;
+
+public abstract class AbstractJDBCAdapterFactory implements AdapterFactory {
+	public AbstractJDBCAdapterFactory() {
+	}
+}
