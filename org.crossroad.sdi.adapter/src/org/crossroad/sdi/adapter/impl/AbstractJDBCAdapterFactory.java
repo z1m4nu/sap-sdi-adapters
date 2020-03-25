5b24fd55baae286b0b9e8a11cef0853d9714ee3b
Manu <e.soden@gmail.com>
2020-03-25 17:22:01 +0100

    Clean

--- org.crossroad.sdi.adapter/src/org/crossroad/sdi/adapter/impl/AbstractJDBCAdapterFactory.java
+++
@@ -1,8 +0,0 @@
-package org.crossroad.sdi.adapter.impl;
-
-import com.sap.hana.dp.adapter.sdk.AdapterFactory;
-
-public abstract class AbstractJDBCAdapterFactory implements AdapterFactory {
-	public AbstractJDBCAdapterFactory() {
-	}
-}
